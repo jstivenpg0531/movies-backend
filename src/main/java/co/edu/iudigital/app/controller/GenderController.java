76cecf6ac6c964dc5539abf8d140b987000ee7c9
jstivenpg0531 <stivengarcia10.sg@gmail.com>
2021-04-16 19:40:23 -0500

    First Commit Backend

---
+++ src/main/java/co/edu/iudigital/app/controller/GenderController.java
@@ -0,0 +1,30 @@
+package co.edu.iudigital.app.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.iudigital.app.model.entity.Gender;
+import co.edu.iudigital.app.service.iface.GenderService;
+
+@RequestMapping("/gender")
+@RestController
+@CrossOrigin("*")
+public class GenderController {
+	
+	//Inyección de dependencias desde el service
+	@Autowired
+	private GenderService genderService;
+	
+	@GetMapping
+	public List<Gender> getAll(){
+		return genderService.getAll();
+		
+		
+	}
+
+}
